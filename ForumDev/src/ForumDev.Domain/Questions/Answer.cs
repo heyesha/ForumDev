81bd1b19e664f0b89fb696b1edf88ed91a635b69
Rodion <rodion.kondrashin@bk.ru>
2025-04-13 16:15:42 +0700

    Done with domain models

---
+++ ForumDev/src/ForumDev.Domain/Questions/Answer.cs
@@ -0,0 +1,16 @@
+﻿namespace ForumDev.Domain.Questions;
+
+public class Answer
+{
+    public Guid Id { get; set; }
+     
+    public required Guid UserId { get; set; }
+     
+    public required string Text { get; set; }
+     
+    public required Question Question { get; set; }
+    
+    public required Answer Parent { get; set; }
+    
+    public List<Guid> Tags { get; set; } = [];
+}
